f070c02b4225502dd52ee99f59c798621a293d5c
Mike Purvis <mikedotexe@gmail.com>
2021-08-20 16:29:34 -0700

    correct comments, use VMContextBuilder for tests

--- src/lib.rs
+++ src/lib.rs
@@ -17,39 +17,24 @@ impl Contract {
 /*
  * the rest of this file sets up unit tests
  * to run these, the command will be:
- * cargo test --package rust-counter-tutorial -- --nocapture
- * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
+ * cargo test --package rust-template -- --nocapture
+ * Note: 'rust-template' comes from cargo.toml's 'name' key
  */
 
 // use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::{testing_env, VMContext};
+    use near_sdk::{MockedBlockchain, testing_env};
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::json_types::ValidAccountId;
 
     // part of writing unit tests is setting up a mock context
-    // in this example, this is only needed for env::log in the contract
-    // this is also a useful list to peek at when wondering what's available in env::*
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-        VMContext {
-            current_account_id: "alice.testnet".to_string(),
-            signer_account_id: "robert.testnet".to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "jane.testnet".to_string(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
+    // provide a `predecessor` here, it'll modify the default context
+    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
     }
 
     // TESTS HERE
