fd113067d6795537b439754a2081de8556f62648
Irene Fabris <irene@balloonbox.io>
2022-04-11 14:06:16 -0700

    mod on .js file

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -54584,7 +54584,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50433" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50636" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
