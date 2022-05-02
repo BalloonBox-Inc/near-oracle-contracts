c79815054825d6c874e5acdb6ebdf3a57650a735
Irene Fabris <irene@balloonbox.io>
2022-05-02 16:55:11 -0700

    minting logic, metadata, init fns

--- nft-contract/src/lib.rs
+++ nft-contract/src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{Base64VecU8, U128};
@@ -6,27 +5,45 @@ use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
     env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
 };
+use std::collections::HashMap;
 
+pub use crate::approval::*;
 pub use crate::metadata::*;
 pub use crate::mint::*;
 pub use crate::nft_core::*;
-pub use crate::approval::*;
 pub use crate::royalty::*;
 
-mod approval; 
-mod enumeration; 
-mod metadata; 
-mod mint; 
-mod nft_core; 
-mod royalty; 
+mod approval;
+mod enumeration;
+mod metadata;
+mod mint;
+mod nft_core;
+mod royalty;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    /*
-        FILL THIS IN
-    */
+    // contract owner
+    pub ower_id: AccountId,
+
+    // token IDs for a given account
+    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+
+    // token struct of a given token ID
+    pub token_by_id: LookupMap<TokenId, Token>,
+
+    // token medatada for a given token ID
+    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
+
+    // metadata for the contract
+    pub metadata: LazyOption<NFTContractMetadata>,
 }
+/*
+Notice: teh 'Contract' struct comprises of some custom data types, which we'll summarize here below:
+AccountId: a string
+TokenId: a string
+Token, TokenMetadata, and NFTContractMetadata: are all structs, defined later
+*/
 
 /// Helper structure for keys of the persistent collections.
 #[derive(BorshSerialize)]
@@ -49,21 +66,48 @@ impl Contract {
         user doesn't have to manually type metadata.
     */
     #[init]
-    pub fn new_default_meta(owner_id: AccountId) {
-        /*
-            FILL THIS IN
-        */
+    pub fn new_default_meta(owner_id: AccountId) -> Self {
+        // calls the other function "new" with some default meradata and the owner_id passed in as the only argument
+        Self::new(
+            owner_id,
+            NFTContractMetadata {
+                spec: "nft-1.0.0".to_string(),
+                name: "Tutorial to mint NFTs on NEAR",
+                symbol: "Balloonbox".to_string(),
+                icon: None,
+                base_url: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
     }
 
     /*
         initialization function (can only be called once).
         this initializes the contract with metadata that was passed in and
-        the owner_id. 
+        the owner_id.
     */
     #[init]
-    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) {
-        /*
-            FILL THIS IN
-        */
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+        // create a variable of type Self initializing all fields
+        let this = Self {
+            // set the owner_id field equal to the passed in owner_id
+            owner_id,
+            metadata: LazyOption::new(
+                StorageKey::NFTContractMetadata.try_to_vec().unwrap(),
+                Some(&metadata),
+            ),
+
+            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner.try_to_vec().unwrap()),
+
+            token_by_id: LookupMap::new(StorageKey::TokensById.try_to_vec().unwrap()),
+
+            token_metadata_by_id: UnorderedMap::new(
+                StorageKey::TokenMetadataById.try_to_vec().unwrap(),
+            ),
+        };
+
+        // return the Contract object
+        this
     }
 }
--- nft-contract/src/mint.rs
+++ nft-contract/src/mint.rs
@@ -9,8 +9,33 @@ impl Contract {
         metadata: TokenMetadata,
         receiver_id: Option<AccountId>,
     ) {
-        /*
-            FILL THIS IN
-        */
+        // measure the initial storage being used on the contract
+        let initial_storage_usage = env::storage_usage();
+
+        // specify the token struct that contains the owner ID
+        let token = Token {
+            // set owner ID to be equal to the receiver ID
+            owner_id: receiver_id,
+        };
+
+        // insert the token ID and the token struct,
+        // but first make sure that the token doen't exist -> do this latter part by using
+        // the 'assert!' macro with a custom panic message
+        assert!(
+            self.token_by_id.insert(&token_id, &token).is_none(),
+            "Token already exists"
+        );
+
+        // insert token id and metadata
+        self.token_metadata_by_id.insert(&token_id, &metadata);
+
+        //call an internal method to add a oken tothe owner
+        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+
+        //calculate the required storage = used storage - initial storage
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+
+        // refund surplus storage to user OR panic if they didn't attach enough to cover for the required gas fee
+        refund_deposit(required_storage_in_bytes);
     }
 }
