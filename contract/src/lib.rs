423816c7d8b163cb69a44f86ef8445b6cab9e631
Irene Fabris <irenefabris@Irenes-MacBook-Air.local>
2022-05-12 12:10:53 -0700

    compile/deploy/interact w/ contract state

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,56 +1,55 @@
+// Import crates
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedSet};
 use near_sdk::{env, near_bindgen};
+
+// --------------------------------------------------------------------- //
+//                          Define main objects                          //
+//                                                                       //
+// ----------------------------------------------------------------------//
+
+const USER_NUMBER: u64 = 2;
 
 // struct on the current state of the smart contract
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct State {
-    pub max_size: u16,
+    pub max_size: u64,
+    pub size_now: u64,
     pub user_count: u64,
     pub score_count: u64,
 }
 
-// user's score, timestamp, and score descriptor as a struct
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct User {
-    pub score: u16,
-    pub timestamp: u32,
-    pub description: Vec<u8>,
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            max_size: 3000,
+            size_now: env::storage_usage(),
+            user_count: USER_NUMBER,
+            score_count: 5,
+        }
+    }
 }
 
-// main struct of the contract storing the all-time scoreboard
+// --------------------------------------------------------------------- //
+//                        Implement main objects                         //
+//                                                                       //
+// ----------------------------------------------------------------------//
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    pub score_board: LookupMap<String, UnorderedSet<User>>,
+impl State {
+    // read the number of user
+    pub fn get_user_number(&self) -> u64 {
+        self.user_count
+    }
+
+    // update the state of the contract
+    pub fn set_state(&mut self, max_size: u64, score_count: u64) {
+        self.max_size = max_size;
+        self.size_now = env::storage_usage();
+        self.user_count = USER_NUMBER + 100;
+        self.score_count = score_count;
+    }
+
+    pub fn get_state(&self) -> String {
+        return String::from("active state");
+    }
 }
-
-// // TBD: AccoundId, which will be a string
-
-// #[near_bindgen]
-// #[derive(BorshDeserialize, BorshSerialize)]
-// pub struct StatusMessage {
-//     records: LookupMap<String, String>,
-// }
-
-// impl Default for StatusMessage {
-//     fn default() -> Self {
-//         Self {
-//             records: LookupMap::new(b"r".to_vec()),
-//         }
-//     }
-// }
-
-// #[near_bindgen]
-// impl StatusMessage {
-//     pub fn set_status(&mut self, message: String) {
-//         let account_id = String::from(env::signer_account_id());
-//         self.records.insert(&account_id, &message);
-//     }
-
-//     pub fn get_status(&self, account_id: String) -> Option<String> {
-//         return self.records.get(&account_id);
-//     }
-// }
