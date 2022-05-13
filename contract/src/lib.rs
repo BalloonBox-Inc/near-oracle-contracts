6434df3879b5b2d54cc545541462896a04a0c9ba
Irene Fabris <irenefabris@Irenes-MacBook-Air.local>
2022-05-13 14:25:01 -0700

    #2 store score for new OR returning user

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,9 @@
 // Import crates
-// use near_sdk::collections::{LookupSet, Vector};
+use near_sdk::collections::{LookupMap, LookupSet, Vector};
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
-    serde::Serialize,
+    serde::{Deserialize, Serialize},
+    AccountId, PanicOnDefault,
 };
 use near_sdk::{env, near_bindgen};
 
@@ -13,9 +14,8 @@ use near_sdk::{env, near_bindgen};
 
 const USER_NUMBER: u64 = 7;
 
-// struct on the current state of the smart contract
-#[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+// on-chain struct describing the current state of the smart contract
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct State {
     pub max_size: u64,
     pub size_now: u64,
@@ -23,21 +23,48 @@ pub struct State {
     pub score_count: u64,
 }
 
+// off-chain Vec<u64> returning the contract state in a human-readable format
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct ContractState {
     state_now: Vec<u64>,
 }
 
+#[derive(Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct MyScoreHistory {
+    scores: Vec<u8>,
+}
+
+// user's score, timestamp, and score descriptor as a struct
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Score {
+    pub score: u16,
+    pub timestamp: u32,
+    pub description: u32,
+}
+
+// // a scoreboard struct containing all scores for one user
+// #[derive(BorshDeserialize, BorshSerialize)]
+// pub struct ScoreBoard {
+//     scores_by_user: LookupSet<Score>,
+// }
+
+// singleton and main struct for this smart contract
+#[near_bindgen]
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    owner_id: AccountId,
+    records: LookupMap<String, LookupSet<Score>>,
+}
+
 // --------------------------------------------------------------------- //
 //                        Implement main objects                         //
 //                                                                       //
 // ----------------------------------------------------------------------//
-#[near_bindgen]
-impl State {
-    // initialize the contract
-    #[init]
-    pub fn new() -> Self {
+impl Default for State {
+    fn default() -> Self {
         Self {
             max_size: 1000000,
             size_now: env::storage_usage(),
@@ -45,30 +72,66 @@ impl State {
             score_count: 0,
         }
     }
+}
 
-    // read the number of user
-    pub fn get_user_number(&self) -> u64 {
-        self.user_count
+#[near_bindgen]
+impl Contract {
+    // initialize the contract
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        Self {
+            owner_id,
+            records: LookupMap::new(b"s"),
+        }
     }
 
-    // update the state of the contract
-    pub fn set_state(&mut self) {
-        // max_size remains equal to its initialization value
-        self.size_now = env::storage_usage();
-        self.user_count += 1;
-        self.score_count += 1;
+    pub fn store_score(&mut self, score: u16, timestamp: u32, description: u32) {
+        let account_id = String::from(env::predecessor_account_id());
+        let new_score = Score {
+            score: score,
+            timestamp: timestamp,
+            description: description,
+        };
+        let mut x = LookupSet::new(b"c");
+        x.insert(&new_score);
+        // let y = ScoreBoard { scores_by_user: x };
+        self.records.insert(&account_id, &x);
     }
 
-    pub fn get_state(&self) -> u64 {
-        return self.size_now;
+    pub fn query_all_scores(&self, account_id: String) -> MyScoreHistory {
+        let all_scores = self.records.get(&account_id);
+        // implement logic in case the above Option<T> returns a NoneType
+        let read_score = all_scores.try_to_vec().unwrap();
+        MyScoreHistory { scores: read_score }
     }
 
-    pub fn read_state(&self) -> ContractState {
-        let mut info = vec![];
-        info.push(self.max_size);
-        info.push(self.size_now);
-        info.push(self.user_count);
-        info.push(self.score_count);
-        ContractState { state_now: info }
-    }
+    // pub fn query_all_scores(&self, account_id: String) -> Option<ScoreBoard> {
+    //     return self.records.get(&account_id);
+    // }
+
+    // // read the number of user
+    // pub fn get_user_number(&self) -> u64 {
+    //     self.user_count
+    // }
+
+    // // update the state of the contract
+    // pub fn set_state(&mut self) {
+    //     // max_size remains equal to its initialization value
+    //     self::size_now = env::storage_usage();
+    //     self.user_count += 1;
+    //     self.score_count += 1;
+    // }
+
+    // pub fn get_state(&self) -> u64 {
+    //     return self.size_now;
+    // }
+
+    // pub fn read_state(&self) -> ContractState {
+    //     let mut info = vec![];
+    //     info.push(self.max_size);
+    //     info.push(self.size_now);
+    //     info.push(self.user_count);
+    //     info.push(self.score_count);
+    //     ContractState { state_now: info }
+    // }
 }
