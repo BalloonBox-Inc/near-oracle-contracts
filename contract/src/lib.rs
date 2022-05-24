ed9fff44996ea661c6128e460970bfa4c389caa3
irene-bbox <irene@balloonbox.io>
2022-05-24 15:26:29 -0700

    #14,#16,#17 no max size, user struct, [payable]

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -3,7 +3,7 @@ use near_sdk::collections::{LookupMap, Vector};
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
     serde::{Deserialize, Serialize},
-    AccountId, PanicOnDefault,
+    AccountId, Gas, PanicOnDefault,
 };
 use near_sdk::{env, near_bindgen};
 
@@ -11,15 +11,9 @@ use near_sdk::{env, near_bindgen};
 //                          Define main objects                          //
 //                                                                       //
 // ----------------------------------------------------------------------//
-
-// static USER_COUNT: u64 = 0;
-// static SCORE_COUNT: u64 = 0;
-// const MAX_SIZE: u64 = 5000000000000;
-
 // on-chain struct describing the current state of the smart contract
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct State {
-    pub max_size: u64,
     pub user_count: u64,
     pub score_count: u64,
 }
@@ -30,33 +24,47 @@ pub struct State {
 pub struct ContractState {
     owner: String,
     timestamp: u64,
-    max_size: u64,
     size_now: u64,
     user_count: u64,
     score_count: u64,
 }
 
+// output of the function querying a user's score history
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct MyScoreHistory {
-    scores: Vec<Score>,
+    scores: Vec<User>,
 }
 
-// user's score, timestamp, and score descriptor as a struct
+// was the operation of publishing a score to blockchain successful?
+// the struct below describes the operation outcome
+#[derive(Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct PublishingOutcome {
+    gas_used: Gas,
+    score_owner: String,
+    successful_operation: bool,
+}
+
+// user's score, timestamp, and score description as a struct
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct Score {
+pub struct User {
     pub score: u16,
     pub timestamp: u64,
     pub description: Vec<u8>,
 }
 
-// singleton and main struct for this smart contract
+// this is the singleton = the main struct for this smart contract
+// the [near_bindgen] macro is used on the singleton only and it generates the boilterplate
+// allowing all the methods implemented on the 'Contract' singleton to be called externally
+// Lastly, use the [PanicOnDefault] macro to prohibit the default implementation on the contract.
+// Since we wrote a custom init function, called new(), we want Default implementation to be prohibited.
 #[near_bindgen]
 #[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     owner_id: AccountId,
-    records: LookupMap<String, Vector<Score>>,
+    records: LookupMap<String, Vector<User>>,
     contract_state: State,
 }
 
@@ -70,11 +78,16 @@ impl Contract {
     // initialize the contract
     #[init]
     pub fn new(owner_id: AccountId) -> Self {
+        // ensure that state doesn't exist.
+        // You should NOT initialize a contract if it's state exists already
+        assert!(
+            !env::state_exists(),
+            "ERR_THE_CONTRACT_IS_ALREADY_INITIALIZED"
+        );
         Self {
             owner_id,
             records: LookupMap::new(b"s"),
             contract_state: State {
-                max_size: 5000000000,
                 user_count: 0u64,
                 score_count: 0u64,
             },
@@ -85,70 +98,57 @@ impl Contract {
     //              Score-related implementations           //
     // -----------------------------------------------------//
     // store a new score to blockchain
-    pub fn store_score(&mut self, score: u16, description: String) -> String {
+    // declare this to be a payable method using the [payable] macro
+    // i.e., you must pay gas to be able to call and execute this function
+    #[payable]
+    pub fn store_score(&mut self, score: u16, description: String) -> PublishingOutcome {
         let account_id = String::from(env::predecessor_account_id());
-        let new_score = Score {
+        let new_score = User {
             score: score,
             timestamp: env::block_timestamp(),
             description: env::sha256(description.as_bytes()),
         };
 
-        // store score iff contract hasn't maxed out its memory upper bound
-        if env::storage_usage() < self.contract_state.max_size {
-            let mappy = self.records.get(&account_id);
+        let mappy = self.records.get(&account_id);
+        match mappy {
+            // if it's a new user --> create a brand new vector to store their score
+            None => {
+                let mut x = Vector::new(b"v");
+                x.push(&new_score);
+                // update the score count iff you succeeded writing it to chain`
+                self.records.insert(&account_id, &x);
+                self.contract_state.user_count += 1;
+                self.contract_state.score_count += 1;
+            }
 
-            match mappy {
-                // if it's a new user --> create a brand new vector to store their score
-                None => {
-                    let mut x = Vector::new(b"v");
-                    x.push(&new_score);
-                    // update the score count iff you succeeded writing it to chain`
-                    self.records.insert(&account_id, &x);
-                    self.contract_state.user_count += 1;
-                    self.contract_state.score_count += 1;
-                }
-
-                // if it's a returning user --> append new score to existing vector
-                Some(i) => {
-                    let indx = i.len() - 1;
-                    if let Some(j) = i.get(indx) {
-                        let timelapsed = new_score.timestamp - j.timestamp;
-                        // if statement w/ 2 conditions: iff there's less than 10 scores, iff last score is 30+ days old
-                        if i.len() < 10 {
-                            // && timelapsed > 30 * u64::pow(10, 9) { // 30 seconds
-                            // && timelapsed > 2592 * u64::pow(10, 12) {  // 30 days
-                            let mut y = i;
-                            y.push(&new_score);
-                            // update the score count iff you succeeded writing it to chain
-                            self.records.insert(&account_id, &y);
-                            self.contract_state.score_count += 1;
-                        } else {
-                            env::panic_str(
-                                "ERR_EXCEEDED_TEN_SCORES_UPPERBOUND_OR_LATEST_SCORE_IS_TOO_RECENT",
-                            )
-                        }
+            // if it's a returning user --> append new score to existing vector
+            Some(i) => {
+                let indx = i.len() - 1;
+                if let Some(j) = i.get(indx) {
+                    let _timelapsed = new_score.timestamp - j.timestamp;
+                    // if statement w/ 2 conditions: iff there's less than 10 scores, iff last score is 30+ days old
+                    if i.len() < 10 {
+                        // && timelapsed > 30 * u64::pow(10, 9) { // 30 seconds
+                        // && timelapsed > 2592 * u64::pow(10, 12) {  // 30 days
+                        let mut y = i;
+                        y.push(&new_score);
+                        // update the score count iff you succeeded writing it to chain
+                        self.records.insert(&account_id, &y);
+                        self.contract_state.score_count += 1;
+                    } else {
+                        env::panic_str(
+                            "ERR_EXCEEDED_TEN_SCORES_UPPERBOUND_OR_LATEST_SCORE_IS_TOO_RECENT",
+                        )
                     }
                 }
             }
-        } else {
-            env::panic_str("ERR_MAXED_OUT_MEMORY")
         }
         // return the account name. This name is the dictionary key to access user's scores
         // iff you successfully stored the score to blockchain, then return the key to access such scores
-        return account_id;
-    }
-
-    // query latest score for a specified user
-    pub fn query_latest_score(&self, account_id: String) -> Score {
-        if let Some(i) = self.records.get(&account_id) {
-            let indx = i.len() - 1;
-            match i.get(indx) {
-                None => env::panic_str("ERR_THIS_USER_HAS_AN_EMPTY_SCORE_RECORD"),
-                Some(j) => return j,
-            }
-        } else {
-            // panic when the account_id has no scores associated to it
-            env::panic_str("ERR_NO_RECORD_FOR_THIS_USER")
+        PublishingOutcome {
+            gas_used: env::used_gas(),
+            score_owner: account_id,
+            successful_operation: true,
         }
     }
 
@@ -173,7 +173,6 @@ impl Contract {
         ContractState {
             owner: String::from(env::current_account_id()),
             timestamp: env::block_timestamp(),
-            max_size: self.contract_state.max_size,
             size_now: env::storage_usage(),
             user_count: self.contract_state.user_count,
             score_count: self.contract_state.score_count,
@@ -185,9 +184,9 @@ impl Contract {
         return self.contract_state.score_count;
     }
 
-    // query contract max size
-    pub fn get_max_size(&self) -> u64 {
-        return self.contract_state.max_size;
+    // check whether a user has a score record
+    pub fn user_exist(&self, account_id: String) -> bool {
+        return self.records.get(&account_id).is_some();
     }
 }
 
@@ -260,7 +259,6 @@ mod tests {
         // check initialization values are correct
         assert_eq!(0, contract.contract_state.user_count);
         assert_eq!(0, contract.contract_state.score_count);
-        assert_eq!(5000000000, contract.get_max_size());
         assert_eq!(
             yoda().to_string(),
             String::from(env::predecessor_account_id())
@@ -271,7 +269,6 @@ mod tests {
         // store first score
         let msg1 = "Congrats, your score is 570 points".to_string();
         let acc1 = contract.store_score(570, msg1);
-        assert_eq!(yoda().to_string(), acc1);
         assert_eq!(1, contract.get_score_count());
         let state1 = contract.read_state();
         assert_eq!(1, state1.user_count, "ERR: should be 1 user");
@@ -295,19 +292,18 @@ mod tests {
         let context = get_context(false, spensa());
         testing_env!(context);
         let mut contract = Contract::new(spensa());
+
+        // store 3 scores to blockchain first
+        let msg3 = "Score of 378";
         contract.store_score(312, "Score of 312".to_string());
         contract.store_score(345, "Score of 345".to_string());
-        contract.store_score(378, "Score of 378".to_string());
+        contract.store_score(378, msg3.to_string());
+    }
 
-        // ensure message got sha256 encrypted
-        let score_history = contract.query_all_scores("spensa.testnet".to_string());
-        let last_score = contract.query_latest_score("spensa.testnet".to_string());
-        assert_eq!(378, last_score.score);
-        let msg3 = "Score of 378";
-        let msg3_sha = env::sha256(msg3.as_bytes());
-        assert_eq!(
-            msg3_sha, last_score.description,
-            "ERR: incorrect sha256 encryption of score descriptions"
-        );
+    fn read_only() {
+        let context = get_context(true, spensa());
+        testing_env!(context);
+        let mut contract = Contract::new(spensa());
+        // write test here
     }
 }
